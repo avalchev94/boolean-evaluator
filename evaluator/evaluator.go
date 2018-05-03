d66d1a52fc6bf96ea35aea30cbc13d2b8bd93a9d
avalchev94 <asen.valchev1@gmail.com>
2018-05-03 18:45:30 +0300

    Boolean evaluator implementations
    Made  a key implementations that almost finishes the boolean-evaluator.

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -2,78 +2,96 @@ package evaluator
 
 import (
 	"fmt"
-	"github.com/avalchev94/boolean-evaluator/stack"
-	"github.com/avalchev94/boolean-evaluator/tree"
 	"strings"
-	"unicode"
+
+	"github.com/avalchev94/boolean-evaluator/stack"
 )
 
 // Evaluator keeps the needed data for evaluating
 type Evaluator struct {
-	tree *tree.Tree
-}
-
-func findBracketMatch(expression string) int {
-	brackets := 1
-
-	for i := 0; i < len(expression); i++ {
-		switch expression[i] {
-		case lbracket:
-			brackets++
-		case rbracket:
-			brackets--
-		}
-
-		if brackets == 0 {
-			return i
-		} else if brackets < 0 {
-			return -1
-		}
-	}
-
-	return -1
-}
-
-func toTree(expression string) (*tree.Tree, error) {
-	termStack := stack.New()
-  operatorStack := stack.New()
-
-	reader := strings.NewReader(expression)
-	for reader.Len() > 0 {
-		r, i, err := reader.ReadRune()
-		if err != nil {
-			break
-		}
-
-		if op := toOperator(r); op != nil {
-      switch operator {
-      case not:
-        if termStack.Len() <= 0 {
-					return nil, fmt.Errorf("Operator \"not\" on column %d does not have term.", i)
-				}
-        newTree := tree.New(not.char)
-        newTree.SetChild(termStack.Pop().(*tree.Tree))
-				term := termStack.Pop().(*tree.Tree)
-				treeStack.Push(tree.New(not.char).SetChild(term))
-
-      case and, or:
-        if op.greater(operatorStack.Top().(*operator)) {
-          if termStack.Len() <= 1 {
-            return nil, fmt.Errorf("Operator %s on column %d does not have enough terms.", op.char, i)
-          }
-
-        }
-      }
-			if r == not.char {
-
-
-		} else if isTerm(r) {
-			termStack.Push(readTerm(reader))
-		}
-	}
+	Parameters map[parameter]bool
+	expression string
 }
 
 func New(expression string) (*Evaluator, error) {
-	tree, err := expressionToTree(expression)
-	return &Evaluator{tree}, err
+	parameters := make(map[parameter]bool)
+
+	reader := strings.NewReader(expression)
+	for reader.Len() > 0 {
+		r, _, err := reader.ReadRune()
+		if err != nil {
+			return nil, fmt.Errorf("failed to read next rune")
+		}
+
+		if param, err := readParameter(r, reader); err == nil {
+			parameters[param] = false
+		}
+	}
+
+	return &Evaluator{parameters, expression}, nil
+}
+
+func (this *Evaluator) Evaluate() (bool, error) {
+	parameters := stack.New()
+	operators := stack.New()
+
+	reader := strings.NewReader(this.expression)
+	for reader.Len() > 0 {
+		r, i, err := reader.ReadRune()
+		if err != nil {
+			return false, fmt.Errorf("failed to read next rune")
+		}
+
+		if op := readOperator(r); op != nil {
+			switch {
+			case op.equal(not):
+				b, err := op.calculate(parameters)
+				if err != nil {
+					return false, fmt.Errorf("Error on column %d: %s", i, err.Error())
+				}
+				parameters.Push(b)
+
+			case op.equal(and) || op.equal(or):
+				if operators.Len() > 0 {
+					prevOp := operators.Top().(*operator)
+					if !prevOp.equal(leftBracket) && !op.greater(prevOp) {
+						b, err := operators.Pop().(*operator).calculate(parameters)
+						if err != nil {
+							return false, fmt.Errorf("Error on column %d: %s", i, err.Error())
+						}
+						parameters.Push(b)
+					}
+				}
+				operators.Push(op)
+
+			case op.equal(leftBracket):
+				operators.Push(op)
+			case op.equal(rightBracket):
+				for operators.Top().(*operator) != leftBracket && operators.Len() > 0 {
+					b, err := operators.Pop().(*operator).calculate(parameters)
+					if err != nil {
+						return false, fmt.Errorf("Error on column %d: %s", i, err.Error())
+					}
+					parameters.Push(b)
+				}
+
+				if operators.Top().(*operator) != leftBracket {
+					return false, fmt.Errorf("Brackets mismatch on column %d.", i)
+				}
+				operators.Pop()
+			}
+		} else if param, err := readParameter(r, reader); err == nil {
+			parameters.Push(this.Parameters[param])
+		}
+	}
+
+	for operators.Len() > 0 {
+		b, err := operators.Pop().(*operator).calculate(parameters)
+		if err != nil {
+			return false, fmt.Errorf("Parsing failed with error: %s", err.Error())
+		}
+		parameters.Push(b)
+	}
+
+	return parameters.Pop().(bool), nil
 }
--- evaluator/operator.go
+++ evaluator/operator.go
@@ -1,35 +1,77 @@
 package evaluator
 
+import (
+	"fmt"
+
+	"github.com/avalchev94/boolean-evaluator/stack"
+)
+
 type operator struct {
 	char     rune
 	priority int8
+	arity    int8
 }
 
 var (
-	or           = operator{'|', 0}
-	and          = operator{'&', 1}
-	not          = operator{'!', 2}
-	leftBracket  = operator{'(', 3}
-	rightBracket = operator{')', 3}
+	or           = &operator{'|', 0, 2}
+	and          = &operator{'&', 1, 2}
+	not          = &operator{'!', 2, 1}
+	leftBracket  = &operator{'(', 3, 0}
+	rightBracket = &operator{')', 3, 0}
 )
 
-func isOperator(r rune) bool {
-	return r == and.char || r == or.char || r == not.char
+func readOperator(r rune) *operator {
+	switch r {
+	case or.char:
+		fmt.Println("Read operator |")
+		return or
+	case and.char:
+		fmt.Println("Read operator &")
+		return and
+	case not.char:
+		fmt.Println("Read operator !")
+		return not
+	case leftBracket.char:
+		fmt.Println("Read operator (")
+		return leftBracket
+	case rightBracket.char:
+		fmt.Println("Read operator )")
+		return rightBracket
+	}
+
+	return nil
 }
 
-func toOperator(r rune) *operator {
-  switch r {
-  case or.char:
-    return &or
-  case and.char:
-    return &and
-  case not.char:
-    return &not
-  }
-
-  return nil
+func (this *operator) greater(op *operator) bool {
+	return this.priority > op.priority
 }
 
-func (left *operator) greater(right* operator) bool {
-  return left.priority > right.priority
+func (this *operator) equal(op *operator) bool {
+	return this.char == op.char
+}
+
+func (this *operator) calculate(parameters *stack.Stack) (bool, error) {
+	fmt.Printf("Calculation for %c\n", this.char)
+	if this.arity <= 0 {
+		return false, fmt.Errorf("operator arity is zero or less")
+	}
+
+	if parameters.Len() < int(this.arity) {
+		return false, fmt.Errorf("operator %c does not have enough parameters", this.char)
+	}
+
+	var result bool
+	switch {
+	case this.equal(not):
+		result = !parameters.Pop().(bool)
+	case this.equal(and):
+		result = parameters.Pop().(bool) && parameters.Pop().(bool)
+	case this.equal(or):
+		result = parameters.Pop().(bool) || parameters.Pop().(bool)
+	default:
+		return false, fmt.Errorf("operator %c does not have calculate algorithm implemented", this.char)
+	}
+	fmt.Printf("Calculation end with result %v\n", result)
+
+	return result, nil
 }
